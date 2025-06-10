88f55f72b1915e0b07ad348e5aa15afc6a132869
tzdorenko <tanyazdorenkoo@gmail.com>
2025-06-10 12:22:56 +0300

    Initial commit

---
+++ task1.js
@@ -0,0 +1,15 @@
+let averageGrade = 94;
+
+if (averageGrade < 60) {
+    console.log("Незадовільно");
+} else if (averageGrade >= 60 && averageGrade <= 70) {
+    console.log("Задовільно");  
+} else if (verageGrade >= 71 && averageGrade <= 80) {
+    console.log("Добре");
+}else if (verageGrade >= 81 && averageGrade <= 90) {
+    console.log("Дуже добре");
+}else if (verageGrade >= 91 && averageGrade <= 100) {
+    console.log("Відмінно");
+} else {
+    console.log("Incorrect value")
+}   
