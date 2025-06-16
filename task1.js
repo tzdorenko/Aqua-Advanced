a6f269af7348583f1eef50104d04f9db8f02f4ca
tzdorenko <tanyazdorenkoo@gmail.com>
2025-06-16 12:42:46 +0300

    Function Declaration, Function Expression, Arrow Function Expression, Consine Arrow Function Expression

--- task1.js
+++ task1.js
@@ -1,5 +1,26 @@
+//Function Declaration//
 function getAreaRectangle (width, height) {
     return width * height;
 }
-
 console.log(getAreaRectangle(5, 10));
+
+
+
+//Function Expression//
+const getAreaRectangle = function (width, height) {
+    return width * height;
+}
+console.log(getAreaRectangle(5, 10));
+
+
+//Arrow Function Expression//
+const getAreaRectangle = (width, height) => {
+    return width * height;
+}
+console.log (getAreaRectangle(5, 10));
+
+
+//Consine Arrow Function Expression//
+const getAreaRectangle = (width, height) => width * height;
+
+console.log (getAreaRectangle(5, 10));
