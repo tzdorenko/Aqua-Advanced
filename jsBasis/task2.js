1b18761c4547607acc46e17689a712caadb17fa4
tzdorenko <tanyazdorenkoo@gmail.com>
2025-06-28 14:37:14 +0300

    update main

--- jsBasis/task2.js
+++
@@ -1,7 +0,0 @@
-const myName = "Tania";
-const secondName = "Mykola";
-const greetings = "Hello" + " " + myName +" " + "and"+ " " + secondName + "!";
-const greetings2 = `Hello ${myName} and hello ${secondName}!`;
-
-console.log(greetings);
-console.log(greetings2);
--- jsBasis/task3.js
+++
@@ -1,5 +0,0 @@
-let age = 25;
-
-let isAdult = age >= 18;
-
-console.log(isAdult);
