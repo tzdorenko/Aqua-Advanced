f8b3810a0fc55783cdc9c4cb7e32be9d3a75fbd7
tzdorenko <tanyazdorenkoo@gmail.com>
2025-06-28 14:35:04 +0300

    HW4 done

---
+++ jsBasis/task2.js
@@ -0,0 +1,7 @@
+const myName = "Tania";
+const secondName = "Mykola";
+const greetings = "Hello" + " " + myName +" " + "and"+ " " + secondName + "!";
+const greetings2 = `Hello ${myName} and hello ${secondName}!`;
+
+console.log(greetings);
+console.log(greetings2);
