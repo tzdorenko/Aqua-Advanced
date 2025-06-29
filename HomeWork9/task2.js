4dde67437804363029ea9f0de07692be0ad3acd4
tzdorenko <tanyazdorenkoo@gmail.com>
2025-06-29 22:20:50 +0300

    HW9 done

---
+++ HomeWork9/task2.js
@@ -0,0 +1,9 @@
+const book = {
+    title: "ATMOSPHERE",
+    author: "Taylor Jenkins Reid",
+    year: "1980"
+};
+
+const { title, author } = book;
+console.log(title);
+console.log(author);
---
+++ HomeWork9/task4.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: "Tetiana",
+    lastName: "Zdorenko",
+    age: 42
+}
+
+person.email = "tanyazdorenkoo@gmail.com";
+
+delete person.age;
+
+console.log(person);
