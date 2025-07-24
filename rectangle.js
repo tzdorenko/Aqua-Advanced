81a0bca48d9b186ca61c795f5c73769962810e88
tzdorenko <tanyazdorenkoo@gmail.com>
2025-07-24 21:26:08 +0300

    updated branch, remove jest, axios from this branch to hw14

--- rectangle.js
+++
@@ -1,8 +0,0 @@
-function rectangleArea(length, width) {
-    if (length <= 0 || width <= 0) {
-        throw new Error('Length and width must be positive numbers.');
-    }
-    return length * width;
-}
-
-module.exports = rectangleArea;
